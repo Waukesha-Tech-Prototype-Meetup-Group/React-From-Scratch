a40bed325e350db3c8493bcce38350a9a3721558
Eric Walter <eromanowski@gmail.com>
2019-10-15 00:06:21 -0500

    add some prettier and stuffs

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    entry: "./src/index.tsx",
-    resolve: {
-        // Add `.ts` and `.tsx` as a resolvable extension.
-        extensions: [".ts", ".tsx"]
-    },
-    module: {
-        rules: [
-            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-            { test: /\.tsx?$/, loader: "ts-loader" }
-        ]
-    }
+  entry: './src/index.tsx',
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+    ],
+  },
 }
